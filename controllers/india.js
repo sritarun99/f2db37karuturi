92723d51403f60a8c89910aa0bb52c994bd61999
SriTarun Karuturi <108359845+sritarun99@users.noreply.github.com>
2022-11-16 16:54:46 -0600

    updated

--- controllers/india.js
+++ controllers/india.js
@@ -91,3 +91,55 @@ exports.india_view_all_Page = async function (req, res) {
         res.send(`{"error": ${err}}`);
     }
 }; 
+// Handle a show one view with id specified by query 
+exports.india_view_one_Page = async function(req, res) { 
+    console.log("single view for id "  + req.query.id) 
+    try{ 
+        result = await India.findById( req.query.id) 
+        res.render('indiadetail',  
+{ title: 'India Detail', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
+// Handle building the view for creating a costume. 
+// No body, no in path parameter, no query. 
+// Does not need to be async 
+exports.india_create_Page =  function(req, res) { 
+    console.log("create view") 
+    try{ 
+        res.render('indiacreate', { title: 'India Create'}); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};
+// Handle building the view for updating a costume. 
+// query provides the id 
+exports.india_update_Page =  async function(req, res) { 
+    console.log("update view for item "+req.query.id) 
+    try{ 
+        let result = await India.findById(req.query.id) 
+        res.render('indiaupdate', { title: 'India Update', toShow: result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+};  
+// Handle a delete one view with id from query 
+exports.india_delete_Page = async function(req, res) { 
+    console.log("Delete view for id "  + req.query.id) 
+    try{ 
+        result = await India.findById(req.query.id) 
+        res.render('indiadelete', { title: 'India Delete', toShow: 
+result }); 
+    } 
+    catch(err){ 
+        res.status(500) 
+        res.send(`{'error': '${err}'}`); 
+    } 
+}; 
--- routes/India.js
+++ routes/India.js
@@ -3,6 +3,15 @@ var router = express.Router();
 const india_controlers= require('../controllers/india'); 
 
 /* GET home page. */
-router.get('/', india_controlers.india_view_all_Page ); 
+router.get('/', india_controlers.india_view_all_Page );
+/* GET detail india page */ 
+router.get('/detail', india_controlers.india_view_one_Page); 
+/* GET create india page */ 
+router.get('/create', india_controlers.india_create_Page); 
+/* GET create update page */ 
+router.get('/update', india_controlers.india_update_Page); 
+/* GET delete india page */ 
+router.get('/delete', india_controlers.india_delete_Page); 
+  
 
 module.exports = router;
